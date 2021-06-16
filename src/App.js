80a75ce098d551cf748cc11e6351ac23e016ba32
Deepanshu Tiwari <deepanshurtiwari@gmail.com>
2021-06-16 13:26:16 +0530

    react-recipe app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
+const App = () => {
+  const API_ID = '5666fa7e';
+  const API_KEY = 'da35236d3a71bbad6871788a5de67a50';
+
+  const [recipes, setRecipe] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('paneer');
+
+  useEffect(() => {
+    getRecipe();
+  }, [query])
+
+  const getRecipe = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
+    const data = await response.json();
+    setRecipe(data.hits);
+    console.log(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form className='search-form' onSubmit={getSearch}>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch} />
+        <button className='search-button' type='submit'>Search</button>
+      </form>
+      <div className='recipes'>
+        {recipes.map(recipe => (<Recipe key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />))}
+      </div>
     </div>
   );
 }
